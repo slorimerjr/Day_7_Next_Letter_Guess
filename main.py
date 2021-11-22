7c70d4e1f2ed9688ecabba01b4f062d19f81b972
Sam Lorimer <slorimerjr@gmail.com>
2021-11-22 17:14:40 +0000

    Initial challenge completion.

--- main.py
+++ main.py
@@ -14,14 +14,18 @@ for _ in range(word_length):
     display += "_"
 
 #TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+while "_" in display:
 
-guess = input("Guess a letter: ").lower()
 
-#Check guessed letter
-for position in range(word_length):
-    letter = chosen_word[position]
-    print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
-    if letter == guess:
-        display[position] = letter
+    guess = input("Guess a letter: ").lower()
 
-print(display)
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    print(display)
+else:
+    print("You win!")
