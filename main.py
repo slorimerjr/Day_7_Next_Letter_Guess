a107f497b35c40242a56064ed938aa94dcc4710f
Sam Lorimer <slorimerjr@gmail.com>
2021-11-22 17:27:50 +0000

    Nothing changed, not sure why push.

--- main.py
+++ main.py
@@ -15,8 +15,6 @@ for _ in range(word_length):
 
 #TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
 while "_" in display:
-
-
     guess = input("Guess a letter: ").lower()
 
     #Check guessed letter
